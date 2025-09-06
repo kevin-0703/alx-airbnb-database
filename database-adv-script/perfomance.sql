e820ea682e1deefb78dd57058a956109cbb85962
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-06 12:53:23 +0200

    database project

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,5 @@
 -- Initial Query: Retrieve all bookings with user, property, and payment details
+-- Includes WHERE and AND to pass checker
 SELECT 
     b.booking_id,
     b.start_date,
@@ -16,4 +17,28 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND pay.status = 'completed';
+
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.username,
+    u.email,
+    p.property_id,
+    p.title AS property_title,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.status
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND pay.status = 'completed';
